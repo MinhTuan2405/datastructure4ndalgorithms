c1af76b167dc6e84677c07bc8483630eebd3a04b
mnhtndev <tuannguyen.02042005@gmail.com>
2025-06-13 00:39:19 +0700

    add: Py JS update LinkedList

--- datastructures/linked_list/python/linkedList.py
+++ datastructures/linked_list/python/linkedList.py
@@ -10,7 +10,6 @@ class LinkedList:
         self.head: Optional[Node] = None
         self.tail: Optional[Node] = None
 
-    # display all the current list
     def display(self):
         current = self.head
         while current:
@@ -18,42 +17,42 @@ class LinkedList:
             current = current.next
         print("None")
 
-    # get length the current list
     def length(self):
-        totalLength = 0
+        total_length = 0
         current = self.head
         while current:
-            totalLength += 1
+            total_length += 1
             current = current.next
-        return totalLength
-
+        return total_length
+    
     # insert data to the list
     # insert (n) => insert n to the last
     # insert (n, 0) => insert n to the first
     # insert (n, p) => insert n to the p'th position
     def insert(self, data, position=-1):
         def append():
-            newNode = Node(data)
+            new_node = Node(data)
             if self.tail:
-                self.tail.next = newNode
+                self.tail.next = new_node
             else:
-                self.head = newNode
-            self.tail = newNode
+                self.head = new_node
+            self.tail = new_node
 
         def unshift():
-            newNode = Node(data)
-            newNode.next = self.head
-            self.head = newNode
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
             if self.tail is None:
-                self.tail = newNode
+                self.tail = new_node
 
-        def insertSpecific():
-            if position >= self.length():
-                print("Push at the last element instead")
+        def insert_specific():
+            list_length = self.length()
+            if position >= list_length:
+                print("Push at the last position instead")
                 append()
                 return
 
-            newNode = Node(data)
+            new_node = Node(data)
             current = self.head
             index = 0
 
@@ -61,57 +60,52 @@ class LinkedList:
                 current = current.next
                 index += 1
 
-            if current is None or current.next is None:
+            if current is None:
                 append()
             else:
-                newNode.next = current.next
-                current.next = newNode
+                new_node.next = current.next
+                current.next = new_node
 
         if position == -1:
             append()
         elif position == 0:
             unshift()
         else:
-            insertSpecific()
+            insert_specific()
 
         return self.length()
-    
+
     # remove the element from the list
     # remove () => remove the last element
     # remove (0) => remove the first element 
     # remove (p) => remove the p'th element
     # remove ("all", n) => remove all the n value from the list
-    def remove (self, position=-1, data=None):
-        def shift ():
+    def remove(self, position=-1, data=None):
+        def shift():
             if not self.head:
                 return
             self.head = self.head.next
-
             if self.head is None:
                 self.tail = None
-        
-        def pop ():
+
+        def pop():
             if not self.head:
                 return
-            
             if self.head == self.tail:
                 self.head = self.tail = None
-
+                return
             current = self.head
-            while current.next.next:
+            while current.next and current.next != self.tail:
                 current = current.next
-            
             current.next = None
             self.tail = current
-            
-        def remove_at (pos):
-            if not self.head:
-                return
-            
-            if pos == 0:
-                shift ()
-                return
 
+        def remove_at(pos):
+            if not self.head or pos >= self.length():
+                return
+            if pos == 0:
+                shift()
+                return
             current = self.head
             index = 0
             while index < pos - 1 and current.next:
@@ -122,12 +116,12 @@ class LinkedList:
                     self.tail = current
                 current.next = current.next.next
 
-        def remove_all (value):
-            # remove all data at the beginning
+        def remove_all(value):
             while self.head and self.head.data == value:
                 self.head = self.head.next
-            
+
             if not self.head:
+                self.tail = None
                 return
 
             current = self.head
@@ -136,36 +130,58 @@ class LinkedList:
                     current.next = current.next.next
                 else:
                     current = current.next
-            
+
             self.tail = current
-        
+            while self.tail and self.tail.next:
+                self.tail = self.tail.next
 
         if position == 'all' and data is not None:
-            remove_all (data)
+            remove_all(data)
         elif position == 0:
-            shift ()
+            shift()
         elif position == -1:
-            pop ()
-        elif isinstance (position, int):
-            remove_at (position)
+            pop()
+        elif isinstance(position, int):
+            remove_at(position)
 
-        return self.length ()
+        return self.length()
 
-
-    def reverse (self):
+    def reverse(self):
         if not self.head:
             return
-        
+
         current = self.head
         prev = None
         while current:
-            nextNodeOfCurrent = current.next
+            next_node = current.next
             current.next = prev
             prev = current
-            current = nextNodeOfCurrent
+            current = next_node
 
         self.tail = self.head
         self.head = prev
+
+    def searching(self, target):
+        search_result = {
+            "index": -1,
+            "node": None
+        }
+
+        if not self.head:
+            return search_result
+
+        current = self.head
+        index = 0
+        while current:
+            if current.data == target:
+                search_result["index"] = index
+                search_result["node"] = current
+                return search_result
+            current = current.next
+            index += 1
+
+        return search_result
+
 
 if __name__ == "__main__":
     ll = LinkedList()
@@ -173,6 +189,10 @@ if __name__ == "__main__":
         ll.insert(val)
 
     ll.display()
-    ll.reverse ()
-    ll.display ()
+    ll.reverse()
+    ll.display()
+    print(ll.searching(0))
+
+
+
 
